0bbee452c12858a9d4ad623b157179589d300482
Ashley Nash <msadoylenash@hotmail.com>
2013-09-12 13:26:02 -0400

    Assignment 5

--- src/DamageThisOne.java
+++ src/DamageThisOne.java
@@ -9,34 +9,32 @@ public class DamageThisOne {
 	 * @param void
 	 */
 	
-	public static void main(String[] args) {
-		//This section declares variables and instantiates the Scanner sc
-		int ticketNumber, stem, checkDigit;
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) 
+	{
+		//This section declares TICKETNUMBER, STEM, CheckDigit
+		int ticket_Number, STEM, check_Digit;
+			Scanner sc = new Scanner(System.in);
 		
-		/* This section requests input from the user, in the form of a 
-		 * six digit number, then it generates a stem and checkdigit 
-		 * from that number
-		 */
-		System.out.print("Please enter a six-digit ticket number: ");
-		ticketNumber = sc.nextInt();
-		stem = ticketNumber / 10;
-		checkDigit = ticketNumber % 10;	
+		// This section requests input from the user, in the form of a six digit number, then it generates a stem and checkdigit from that number
+		 
+		System.out.print("Please enter a six-digit ticket number:");
+			ticket_Number = sc.nextInt();
+			STEM = ticket_Number / 10;
+		check_Digit = ticket_Number / 10;	
 
-		/* This section will check the validity of the ticket number
-		 * and continue to prompt the user for a new ticket number
-		 * while the current number is invalid
-		 */
-		while (checkDigit != stem % 7) {
-			System.out.println("That is NOT a valid ticket number, bucko!");
-			System.out.print("Try again: ");
-			ticketNumber = sc.nextInt();
-			stem = ticketNumber / 10;
-			checkDigit = ticketNumber % 10;	
-		}
-						
+		// This section will check the validity of the ticket number and continue to prompt the user for a new ticket number while the current number is invalid
+		 
+		while (check_Digit != STEM % 7) 
+		{ System.out.println("That is NOT a valid ticket number, bucko!");
+			System.out.print("Try again:");
+		ticket_Number = sc.nextInt();
+			STEM = ticket_Number / 10;
+				check_Digit = ticket_Number % 10;	
+				}
+		
+		/*This line prints the valid ticket number*/				
 		System.out.println("That is a valid ticket number, well done!");
 	
-	}
+			}
 
 }
